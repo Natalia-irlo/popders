63341cedab03855e849481afa161410d9d6fbcb8
DianaB177 <anaidb77@gmail.com>
2023-02-03 19:58:54 +0100

    update

--- back-end/services/connection/crud-update.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-
-function updateRow($connection, $rowID, $title, $artist, $genre, $url, $date, $status)
-{ ///incompletooooo!!!
-    $updateQuery = ' UPDATE song
-    SET  = value1, column2 = value2, ...
-    WHERE condition';
-}
