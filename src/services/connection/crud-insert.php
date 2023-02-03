24b5de510a3a1cbd48a13fc2ae551d4f870ee3e9
SharonInfante <sharonshabel@gmail.com>
2023-02-03 10:30:09 +0100

    pull origin database-connection

--- src/services/connection/crud-insert.php
+++ src/services/connection/crud-insert.php
@@ -3,7 +3,7 @@ include 'connection.php';
 
 function addContent($connection)
 {
-    $addRowQuery = 'INSERT INTO song (title, artist, genre, url, date, status, img) VALUES ("Flowers", "Miley Cirus", "Genre", "hahahah", "05/07/2020", "no", "hahah")';
+    $addRowQuery = 'INSERT INTO song (`id_coder`, title, artist, genre, url, date, status, img) VALUES (1, "Flowers", "Miley Cirus", "Genre", "hahahah", "05/07/2020", "no", "hahah")';
     $connection->query($addRowQuery);
 }
 
