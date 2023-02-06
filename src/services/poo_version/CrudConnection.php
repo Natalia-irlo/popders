0ab1f4dccf01a11d390cc1d4bd84d2dd87bd18c8
Gabriela M. Fernández <galalou2010@gmail.com>
2023-02-06 14:58:49 +0100

    ready songs crud using poo

---
+++ src/services/poo_version/CrudConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+// Esta sería como una interfaz
+
+abstract class CrudConnection
+{
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = $this->connectDatabase();
+    }
+
+    private function connectDatabase()
+    {
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=popders", "root", "");
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    abstract function getRows();
+    // abstract function addRow();
+    // abstract function updateRow();
+    // abstract function deleteRow();
+}
